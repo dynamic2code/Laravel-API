0666d691973b3b579648e79e86e255a16d237907
Joseph Wakahu Ngacha <83699448+dynamic2code@users.noreply.github.com>
2022-12-12 23:40:41 +0300

    changes to api return data

--- your-project-name/routes/api.php
+++ your-project-name/routes/api.php
@@ -17,3 +17,20 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('data', function() {
+    // input will be location and crop you want to search
+    // data will include the location, crop, price, projected price, price trajectory
+    $location = "location";
+    $crop = "crop";
+    $price = "price";
+    $projectedPrice = "projected price";
+    $priceTrajectory = "price trajectory";
+
+    $data = ["location"=>$location,
+             "crop"=> $crop,
+             "currentPrice"=>$price,
+             "projectedPrice"=> $projectedPrice,
+             "price trajectory"=> $priceTrajectory
+        ];
+    return $data;
+});
