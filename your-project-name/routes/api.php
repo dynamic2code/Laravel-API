6a5e3b1de5f07e2818b0dffdaf9c42862acfc7ba
Joseph Wakahu Ngacha <83699448+dynamic2code@users.noreply.github.com>
2022-12-13 21:04:40 +0300

    changes to routing

--- your-project-name/routes/api.php
+++ your-project-name/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\APIController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,24 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('data', function() {
-    // input will be location and crop you want to search
-    // data will include the location, crop, price, projected price, price trajectory
-    $location = "location";
-    $crop = "crop";
-    $price = "price";
-    $projectedPrice = "projected price";
-    $priceTrajectory = "price trajectory";
+Route::get('/data',[APIController::class,'getData']);
 
-    $data = ["location"=>$location,
-             "crop"=> $crop,
-             "currentPrice"=>$price,
-             "projectedPrice"=> $projectedPrice,
-             "price trajectory"=> $priceTrajectory
-        ];
-    return $data;
-});
-
-Route::post('data', function(){
-    
-});
+Route::post('/input', [APIController::class,'postData']);
